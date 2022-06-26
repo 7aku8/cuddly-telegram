47165663adcec30bdabb63bc8a5cb63b458f807f
JakubWolak <jakubwolak.123@gmail.com>
2022-06-26 17:26:36 +0200

    add basic cognito config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from '@modules/shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '@modules/auth/auth.module';
+
+type TEnvironmentName = 'production' | 'staging' | 'development';
+const getEnvFileName = (nodeEnv: TEnvironmentName) => {
+  switch (nodeEnv) {
+    case 'production':
+      return '.env.production';
+    case 'staging':
+      return '.env.staging';
+    case 'development':
+      return '.env.development';
+  }
+};
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    SharedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvFileName(<TEnvironmentName>process.env.NODE_ENV),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
